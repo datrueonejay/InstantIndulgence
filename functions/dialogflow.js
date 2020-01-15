882822b68d73617c1406f8e671fe0e7b0b004955
Jayden Arquelada <jayarquelada@gmail.com>
2020-01-14 23:32:27 -0500

    Worked on location permissions

--- functions/dialogflow.js
+++ functions/dialogflow.js
@@ -1,4 +1,4 @@
-const { dialogflow } = require("actions-on-google");
+const { dialogflow, Permission, Confirmation } = require("actions-on-google");
 
 const { getCuisines } = require("./zomato");
 
@@ -6,18 +6,52 @@ const app = dialogflow();
 
 app.intent("Default Welcome Intent", conv => {
   conv.ask("Welcome to Instant Indulgence, from firebase");
+  conv.ask(
+    new Permission({
+      context: "To locate restaraunts near you.",
+      permissions: "DEVICE_PRECISE_LOCATION"
+    })
+  );
 });
 
 app.intent("Cuisines", conv => {
-  return getCuisines(1, 1)
+  if (conv.data.cuisines) {
+    conv.ask(`Already found from before. The different cuisines are ${conv.data.cuisines}`);
+    return;
+  }
+
+  // Default coordinates for Toronto
+  let latitude = 43.6532;
+  let longitude = -79.3832;
+
+  if (conv.device.location) {
+    ({ latitude, longitude } = conv.device.location.coordinates);
+  }
+  console.log(latitude);
+  console.log(longitude);
+
+  return getCuisines(latitude, longitude)
     .then(res => {
-      console.log(res);
+      conv.data.cuisines = res;
+
       conv.ask(`The different cuisines are ${res}`);
       return;
     })
     .catch(err => {
       throw err;
     });
+});
+
+app.intent("Location", (conv, input, granted) => {
+  if (granted) {
+    conv.ask("Great! Let's find some restaraunts near you!");
+    console.log(conv.device.location);
+  } else {
+    conv.ask(
+      "No problem! If you do want to give your location for more accurate restaraunt recommendations, just let me know!"
+    );
+    console.log(conv.device.location);
+  }
 });
 
 app.intent("Goodbye", conv => {
