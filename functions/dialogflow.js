3e319f6f90094c3857ffa1b9387c18607a478655
Jayden Arquelada <jayarquelada@gmail.com>
2020-05-24 14:15:13 -0400

    Modified location logic

--- functions/dialogflow.js
+++ functions/dialogflow.js
@@ -4,57 +4,76 @@ const { getCuisines } = require("./zomato");
 
 const app = dialogflow();
 
-app.intent("Default Welcome Intent", conv => {
+app.intent("Default Welcome Intent", (conv) => {
   conv.ask("Welcome to Instant Indulgence, from firebase");
-  conv.ask(
+  return conv.ask(
     new Permission({
-      context: "To locate restaraunts near you.",
-      permissions: "DEVICE_PRECISE_LOCATION"
+      context: "To locate restaraunts near you",
+      permissions: "DEVICE_PRECISE_LOCATION",
     })
   );
 });
 
-app.intent("Cuisines", conv => {
+app.intent("Cuisines", (conv) => {
   if (conv.data.cuisines) {
     conv.ask(`Already found from before. The different cuisines are ${conv.data.cuisines}`);
     return;
   }
 
-  // Default coordinates for Toronto
-  let latitude = 43.6532;
-  let longitude = -79.3832;
+  let latitude = null;
+  let longitude = null;
 
   if (conv.device.location) {
     ({ latitude, longitude } = conv.device.location.coordinates);
   }
-  console.log(latitude);
-  console.log(longitude);
 
   return getCuisines(latitude, longitude)
-    .then(res => {
+    .then((res) => {
       conv.data.cuisines = res;
 
       conv.ask(`The different cuisines are ${res}`);
       return;
     })
-    .catch(err => {
+    .catch((err) => {
       throw err;
     });
 });
 
-app.intent("Location", (conv, input, granted) => {
-  if (granted) {
-    conv.ask("Great! Let's find some restaraunts near you!");
-    console.log(conv.device.location);
-  } else {
-    conv.ask(
-      "No problem! If you do want to give your location for more accurate restaraunt recommendations, just let me know!"
-    );
-    console.log(conv.device.location);
-  }
+app.intent("Location Intent", (conv, input) => {
+  return conv.ask(
+    new Permission({
+      context: "To locate restaraunts near you",
+      permissions: "DEVICE_PRECISE_LOCATION",
+    })
+  );
 });
 
-app.intent("Goodbye", conv => {
+app.intent("Location Followup", (conv, input, granted) => {
+  let latitude = null;
+  let longitude = null;
+
+  if (conv.device.location) {
+    ({ latitude, longitude } = conv.device.location.coordinates);
+  }
+  return getCuisines(latitude, longitude)
+    .then((res) => {
+      // Store the cuisines near
+      conv.data.cuisines = res;
+      if (granted) {
+        conv.ask("Great! Let's find some restaraunts near you!");
+      } else {
+        conv.ask(
+          "No problem! If you do want to give your location for more accurate restaraunt recommendations, just let me know!"
+        );
+      }
+      return;
+    })
+    .catch((err) => {
+      throw err;
+    });
+});
+
+app.intent("Goodbye", (conv) => {
   conv.close("Goodbye!");
 });
 
@@ -63,7 +82,7 @@ app.catch((conv, error) => {
   conv.ask("Sorry there was a glitch. Can you say that again? From firebase");
 });
 
-app.intent("Default Fallback Intent", conv => {
+app.intent("Default Fallback Intent", (conv) => {
   conv.ask(`I didn't understand. Can you try again? From firebase`);
 });
 
--- functions/zomato.js
+++ functions/zomato.js
@@ -1,39 +1,42 @@
 const request = require("request-promise");
 const { ZOMATO_URL, ZOMATO_KEY } = require("./constants");
 
+// Default coordinates for Toronto
+let defaultLat = 43.6532;
+let defaultLong = -79.3832;
+
+// TODO: Move and deploy zomato calls to backend api, also add redis cache
+
 const CUISINES_PATH = "cuisines";
 
 function getOptions(path, queryStrings, headers) {
   return {
     url: `${ZOMATO_URL}/${path}`,
     qs: {
-      ...queryStrings
+      ...queryStrings,
     },
     headers: {
       "user-key": ZOMATO_KEY,
-      ...headers
+      ...headers,
     },
-    json: true
+    json: true,
   };
 }
 
 const getCuisines = (lat, long) => {
-  return request(getOptions("/cuisines", (headers = { lat: lat, lon: long })))
-    .then(res => {
+  return request(getOptions("/cuisines", (headers = { lat: lat ? lat : defaultLat, lon: long ? long : defaultLong })))
+    .then((res) => {
       let cuisines = [];
-      res.cuisines.forEach(cuisine => {
-        console.log(cuisine);
+      res.cuisines.forEach((cuisine) => {
         cuisines.push(cuisine["cuisine"]["cuisine_name"]);
       });
-      console.log("CUISINES ARE");
-      console.log(cuisines);
       return cuisines;
     })
-    .catch(err => {
+    .catch((err) => {
       throw err;
     });
 };
 
 module.exports = {
-  getCuisines: getCuisines
+  getCuisines: getCuisines,
 };
